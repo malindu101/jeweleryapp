074d50722530084b5aab2c1b8d3ba66c4d8bfc83
malindu101 <92214511+malindu101@users.noreply.github.com>
2025-06-18 18:18:24 +0530

    Update silver.py

--- silver.py
+++ silver.py
@@ -14,7 +14,7 @@ st.title("🥈 Silver Live Price Forecasting")
 def get_data_from_snowflake():
     conn = snowflake.connector.connect(
         user="MOW101",
-        password="Killme@20021128123123",
+        password="",
         account="KWLEACZ-DX82931",
         warehouse="COMPUTE_WH",
         database="SAPPHIRE",
