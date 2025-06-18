f9d653292a9a54663455d28f373a63d5ca309d0e
malindu101 <92214511+malindu101@users.noreply.github.com>
2025-06-18 18:17:49 +0530

    Update new.py

--- new.py
+++ new.py
@@ -14,7 +14,7 @@ st.title("🔧 Material Usage Forecasting")
 def load_material_data():
     conn = snowflake.connector.connect(
         user="MOW101",
-        password="Killme@20021128123123",
+        password="",
         account="KWLEACZ-DX82931",
         warehouse="COMPUTE_WH",
         database="SAPPHIRE",         # Same DB
